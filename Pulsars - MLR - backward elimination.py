c9c855d49da427e16b51d6c3345f45b1fe4cab47
Dootix <bad.dootix@gmail.com>
2018-04-04 13:02:38 +0200

    Update Pulsars - MLR - backward elimination.py

--- Pulsars - MLR - backward elimination.py
+++ Pulsars - MLR - backward elimination.py
@@ -1,7 +1,6 @@
 """
      PULSAR DISTANCE MEASUREMENT - MULTIPLE LINEAR REGRESSION MODEL
 """
-
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -11,11 +10,9 @@ dataset = pd.read_csv('Pulsars.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 4].values
    
-
 # Data split
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
-
 
 # Fitting multiple linear regression
 from sklearn.linear_model import LinearRegression
@@ -58,7 +55,3 @@ regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
 regressor_OLS.summary()
 
 """***      BACKWARD ELIMINATION    ***"""
-
-
-
-
